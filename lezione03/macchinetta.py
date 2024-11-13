c5f5f6ab5d490bd0c8a2294497bf736dd3b6884e
Enaip Piemonte <mattia.folcarelli@enaip.piemonte.it>
2024-11-13 13:59:40 +0100

    ff

--- lezione03/macchinetta.py
+++ lezione03/macchinetta.py
@@ -5,15 +5,15 @@ print("1) Cherry cola 1$")
 print("2) Ramune 2$")
 print("3) Bubble tea 3$")
 selezione = int(input("SELEZIONE: "))
+
 if selezione == 1:
     print("hai scelto la cherry cola!")
     soldi = soldi - 1 
-    print("Hai ancora " + str(soldi) + "$")
 if selezione == 2:
     print("hai scelto la ramune!")
     soldi = soldi - 2
-    print("Hai ancora " + str(soldi) + "$")
 if selezione == 3:
     print("hai scelto il bubble tea!")
     soldi = soldi - 3
-    print("Hai ancora " + str(soldi) + "$")
+
+print("Hai ancora " + str(soldi) + "$")
