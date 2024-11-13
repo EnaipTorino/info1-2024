21f00ff13a2970d9e8bbb84285f8a7959b0c2540
Enaip Piemonte <mattia.folcarelli@enaip.piemonte.it>
2024-11-13 13:49:20 +0100

    macchinetta

--- lezione03/test.py
+++ lezione03/test.py
@@ -33,28 +33,12 @@ if risposta == "b":
 if risposta == "c":
     zombie = zombie + 2
 print(spaventato, gore, zombie)
+
 '''
 if zombie > spaventato:
     print("Ti consiglio the walking dead")
 else:
     print("Ti consiglio peppa pig")
-'''
-'''
-a = 0
-b = 1
-c = 2
-
-# massimo tra a, b e c
-if a>b:
-    if a>c:
-        print("a è il maggiore")
-    else:
-        print("c è il maggiore")
-else:
-    if b>c:
-        print("a è il maggiore")
-    else:
-        print("c è il maggiore")  
 '''
 a = 0
 b = 1
